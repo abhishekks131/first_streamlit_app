456afcbba479e3b83d39258d32dbdffe6a55b1fb
abhishekks131 <142167171+abhishekks131@users.noreply.github.com>
2023-08-13 21:38:27 +0530

    Update streamlit_app.py
    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('pancakes')
-streamlit.text('Oats')
-streamlit.text('Egg salad ')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 
