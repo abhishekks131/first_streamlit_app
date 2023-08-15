3d477034f415ef83dbd4a87da908f015bca72ff5
abhishekks131 <142167171+abhishekks131@users.noreply.github.com>
2023-08-15 21:41:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,11 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# take the jason version of form and normailze it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output in table form
+streamlit.dataframe(fruityvice_normalized)
+
 
 
   
